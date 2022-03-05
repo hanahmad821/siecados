849fc8590d3a026b5082dd768e302bbd6ab936dd
hanahmad821 <hanahmad821@gmail.com>
2022-03-05 17:39:43 +0700

    menambahkan menu bidang di sidebar

--- application/views/_partials/sidebar.php
+++ application/views/_partials/sidebar.php
@@ -3,19 +3,51 @@
 <div class="main-menu menu-fixed menu-dark menu-accordion menu-shadow" data-scroll-to-active="true" data-img="<?php echo base_url();?>app-assets/images/backgrounds/04.jpg">
       <div class="navbar-header">
         <ul class="nav navbar-nav flex-row position-relative">       
-          <li class="nav-item mr-auto"><a class="navbar-brand" href="<?php echo base_url();?>index.html"><img class="brand-logo" alt="Chameleon admin logo" src="<?php echo base_url();?>app-assets/images/logo/logo.png"/>
-              <h3 class="brand-text">Chameleon</h3></a></li>
+          <li class="nav-item mr-auto"><a class="navbar-brand" href="<?php echo base_url();?>index.html"><img class="brand-logo" alt="Chameleon admin logo" src="<?php echo base_url();?>app-assets/images/logo/logo-binus.png"/>
+              <h3 class="brand-text">Siecados</h3></a></li>
           <li class="nav-item d-none d-md-block nav-toggle"><a class="nav-link modern-nav-toggle pr-0" data-toggle="collapse"><i class="toggle-icon ft-disc font-medium-3" data-ticon="ft-disc"></i></a></li>
           <li class="nav-item d-md-none"><a class="nav-link close-navbar"><i class="ft-x"></i></a></li>
         </ul>
       </div>
       <div class="main-menu-content">
         <ul class="navigation navigation-main" id="main-menu-navigation" data-menu="menu-navigation">
-          <li class=" nav-item"><a href="<?php echo base_url();?>index.html"><i class="ft-home"></i><span class="menu-title" data-i18n="">Dashboard</span><span class="badge badge badge-info badge-pill float-right mr-2">3</span></a>
+          <li class=" nav-item"><a href="<?php echo base_url();?>index.html"><i class="ft-book"></i><span class="menu-title" data-i18n="">Dashboard</span><span class="badge badge badge-info badge-pill float-right mr-2">3</span></a>
             <ul class="menu-content">
               <li class="active"><a class="menu-item" href="<?php echo base_url();?>dashboard-ecommerce.html">eCommerce</a>
               </li>
               <li><a class="menu-item" href="<?php echo base_url();?>dashboard-analytics.html">Analytics</a>
+              </li>
+            </ul>
+          </li>
+          <li class=" nav-item"><a href="<?php echo base_url();?>index.html"><i class="ft-book"></i><span class="menu-title" data-i18n="">Bidang A</span></a>
+            <ul class="menu-content">
+              <li class="active"><a class="menu-item" href="<?php echo base_url();?>index.php/BidangA">Form Input</a>
+              </li>
+              <li><a class="menu-item" href="<?php echo base_url();?>dashboard-analytics.html">Table</a>
+              </li>
+            </ul>
+          </li>
+          <li class=" nav-item"><a href="<?php echo base_url();?>index.html"><i class="ft-book"></i><span class="menu-title" data-i18n="">Bidang B</span></a>
+            <ul class="menu-content">
+              <li class="active"><a class="menu-item" href="<?php echo base_url();?>index.php/BidangB">Form Input</a>
+              </li>
+              <li><a class="menu-item" href="<?php echo base_url();?>dashboard-analytics.html">Table</a>
+              </li>
+            </ul>
+          </li>
+          <li class=" nav-item"><a href="<?php echo base_url();?>index.html"><i class="ft-book"></i><span class="menu-title" data-i18n="">Bidang C</span></a>
+            <ul class="menu-content">
+              <li class="active"><a class="menu-item" href="<?php echo base_url();?>index.php/BidangC">Form Input</a>
+              </li>
+              <li><a class="menu-item" href="<?php echo base_url();?>dashboard-analytics.html">Table</a>
+              </li>
+            </ul>
+          </li>
+          <li class=" nav-item"><a href="<?php echo base_url();?>index.html"><i class="ft-book"></i><span class="menu-title" data-i18n="">Bidang D</span></a>
+            <ul class="menu-content">
+              <li class="active"><a class="menu-item" href="<?php echo base_url();?>index.php/BidangD">Form Input</a>
+              </li>
+              <li><a class="menu-item" href="<?php echo base_url();?>dashboard-analytics.html">Table</a>
               </li>
             </ul>
           </li>
