e86756fcb8aa22b884fb4e76be4d8ea877408c62
hanahmad821 <hanahmad821@gmail.com>
2022-03-30 18:57:57 +0700

    menambahkan library js

--- application/views/_partials/footer.php
+++ application/views/_partials/footer.php
@@ -1,7 +1,30 @@
-<div class="clearfix blue-grey lighten-2 text-sm-center mb-0 px-2"><span class="float-md-left d-block d-md-inline-block">2018  &copy; Copyright <a class="text-bold-800 grey darken-2" href="<?php echo base_url();?>https://themeselection.com" target="_blank">ThemeSelection</a></span>
-        <ul class="list-inline float-md-right d-block d-md-inline-blockd-none d-lg-block mb-0">
-          <li class="list-inline-item"><a class="my-1" href="<?php echo base_url();?>https://themeselection.com/" target="_blank"> More themes</a></li>
-          <li class="list-inline-item"><a class="my-1" href="<?php echo base_url();?>https://themeselection.com/support" target="_blank"> Support</a></li>
-          <li class="list-inline-item"><a class="my-1" href="<?php echo base_url();?>https://themeselection.com/products/chameleon-admin-modern-bootstrap-webapp-dashboard-html-template-ui-kit/" target="_blank"> Purchase</a></li>
-        </ul>
-      </div>
+<div class="clearfix blue-grey lighten-2 text-sm-center mb-0 px-2"><span class="float-md-left d-block d-md-inline-block">2018 &copy; Copyright <a class="text-bold-800 grey darken-2" href="<?php echo base_url(); ?>https://themeselection.com" target="_blank">ThemeSelection</a></span>
+  <ul class="list-inline float-md-right d-block d-md-inline-blockd-none d-lg-block mb-0">
+    <li class="list-inline-item"><a class="my-1" href="<?php echo base_url(); ?>https://themeselection.com/" target="_blank"> More themes</a></li>
+    <li class="list-inline-item"><a class="my-1" href="<?php echo base_url(); ?>https://themeselection.com/support" target="_blank"> Support</a></li>
+    <li class="list-inline-item"><a class="my-1" href="<?php echo base_url(); ?>https://themeselection.com/products/chameleon-admin-modern-bootstrap-webapp-dashboard-html-template-ui-kit/" target="_blank"> Purchase</a></li>
+  </ul>
+</div>
+
+<!-- BEGIN VENDOR JS-->
+<script src="<?php echo base_url(); ?>app-assets/vendors/js/vendors.min.js" type="text/javascript"></script>
+<!-- BEGIN VENDOR JS-->
+<!-- BEGIN PAGE VENDOR JS-->
+<script src="<?php echo base_url(); ?>app-assets/vendors/js/forms/repeater/jquery.repeater.min.js" type="text/javascript"></script>
+<script src="<?php echo base_url(); ?>app-assets/vendors/js/forms/icheck/icheck.min.js" type="text/javascript"></script>
+<script src="<?php echo base_url(); ?>app-assets/vendors/js/pickers/pickadate/picker.js" type="text/javascript"></script>
+<script src="<?php echo base_url(); ?>app-assets/vendors/js/pickers/pickadate/picker.date.js" type="text/javascript"></script>
+<script src="<?php echo base_url(); ?>app-assets/vendors/js/pickers/pickadate/picker.time.js" type="text/javascript"></script>
+<script src="<?php echo base_url(); ?>app-assets/vendors/js/pickers/pickadate/legacy.js" type="text/javascript"></script>
+<script src="<?php echo base_url(); ?>app-assets/vendors/js/pickers/dateTime/moment-with-locales.min.js" type="text/javascript"></script>
+<script src="<?php echo base_url(); ?>app-assets/vendors/js/pickers/daterange/daterangepicker.js" type="text/javascript"></script>
+<!-- END PAGE VENDOR JS-->
+<!-- BEGIN CHAMELEON  JS-->
+<script src="<?php echo base_url(); ?>app-assets/js/core/app-menu.js" type="text/javascript"></script>
+<script src="<?php echo base_url(); ?>app-assets/js/core/app.js" type="text/javascript"></script>
+<!-- END CHAMELEON  JS-->
+<!-- BEGIN PAGE LEVEL JS-->
+<script src="<?php echo base_url(); ?>app-assets/js/scripts/forms/form-repeater.js" type="text/javascript"></script>
+    <script src="<?php echo base_url(); ?>app-assets/js/scripts/forms/checkbox-radio.js" type="text/javascript"></script>
+    <script src="<?php echo base_url(); ?>app-assets/js/scripts/pickers/dateTime/pick-a-datetime.js" type="text/javascript"></script>
+<!-- END PAGE LEVEL JS-->
