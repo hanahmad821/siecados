3a297c2934ff57bfdac5376989dce5246d57b14d
hanahmad821 <hanahmad821@gmail.com>
2022-03-30 18:57:11 +0700

    edit form bidang a

--- application/views/bidangA.php
+++ application/views/bidangA.php
@@ -75,65 +75,66 @@
                                 </div>
                                 <div class="card-content collapse show">
                                     <div class="card-body">
+                                        <div class="col-md-12">
+                                            <form class="form">
+                                                <!-- <label class="col-md-2 label-control" for="projectinput5">Kode Matkul</label> -->
+                                                <div class="form-group">
+                                                    <!-- <input type="text" class="form-control" placeholder="Kode Matkul" name="first"> -->
+                                                    <label for="kode_mk">Mata Kuliah</label>
 
-                                        <form class="form row">
-                                            <!-- <label class="col-md-2 label-control" for="projectinput5">Kode Matkul</label> -->
-                                            <div class="form-group col-md-12 mb-2">
-                                                <!-- <input type="text" class="form-control" placeholder="Kode Matkul" name="first"> -->
-                                                <select id="kode_mk" name="kode_mk" class="form-control">
-                                                    <?php foreach ($kode_mk as $mk) { ?>
-                                                        <option value="<?php echo $mk['mata_kuliah']; ?>"><?php echo $mk['mata_kuliah']; ?> </option>
-                                                    <?php } ?>
-                                                    <option value="none" selected="" disabled="">Kode Matkul</option>
-                                                    <option value="ISYS6327035">ISYS6327035 - Thesis (lanjut)</option>
-                                                    <option value="ISYS6296038">ISYS6296038 - Information Systems Concept</option>
-                                                    <option value="ISYS6299035">ISYS6299035 - Business Process Fundamental</option>
-                                                    <option value="ISYS6300035">ISYS6300035 - Enterprise Systems</option>
-                                                    <option value="ISYS6305035">ISYS6305035 - Data and Information Management</option>
-                                                </select>
-                                            </div>
-                                            <div class="form-group col-md-12 mb-2">
-                                                <input type="text" class="form-control" placeholder="SKS" name="sks">
-                                            </div>
-                                            <div class="form-group col-md-12 mb-2">
-                                                <input type="text" class="form-control" placeholder="KELAS" name="kelas">
-                                            </div>
-                                            <div class="form-group col-12 mb-2">
-                                                <input type="text" class="form-control" name="dosen1" placeholder="NAMA DOSEN" disabled>
-                                            </div>
-                                            <div class="form-group col-md-12 mb-2">
-                                                <!-- <input type="text" class="form-control" placeholder="Kode Matkul" name="first"> -->
-                                                <select id="dosen2" name="dosen2" class="form-control">
-                                                    <option value="none" selected="" disabled="">Dosen 2</option>
-                                                    <option value="ISYS6327035">D4567 - Titan, S.Kom., MMSI.</option>
-                                                    <option value="ISYS6296038">D5001 - Hendro, S.Kom., M.M</option>
-                                                    <option value="ISYS6299035">D5088 - Dr. Feri Sulianta, S.T., M.T.I</option>
-                                                    <option value="ISYS6300035">ISYS6300035 - Enterprise Systems</option>
-                                                    <option value="ISYS6305035">ISYS6305035 - Data and Information Management</option>
-                                                </select>
-                                            </div>
-                                            <div class="form-group col-12 mb-2 file-repeater">
-                                                <div data-repeater-list="repeater-list">
-                                                    <div data-repeater-item>
-                                                        <div class="row mb-1">
-                                                            <div class="col-9 col-xl-10">
-                                                                <input type="text" class="form-control" placeholder="Phone No." name="phone">
-                                                            </div>
-                                                            <div class="col-2 col-xl-1">
-                                                                <button type="button" data-repeater-delete class="btn btn-icon btn-danger mr-1">
-                                                                    <i class="ft-x"></i>
-                                                                </button>
-                                                            </div>
+                                                    <select id="kode_mk" name="kode_mk" class="form-control">
+                                                        <?php foreach ($kode_mk as $mk) { ?>
+                                                            <option value="<?php echo $mk['mata_kuliah']; ?>"><?php echo $mk['mata_kuliah']; ?> </option>
+                                                        <?php } ?>
+                                                        <option value="none" selected="" disabled="">Kode Matkul</option>
+                                                        <option value="ISYS6327035">ISYS6327035 - Thesis (lanjut)</option>
+                                                        <option value="ISYS6296038">ISYS6296038 - Information Systems Concept</option>
+                                                        <option value="ISYS6299035">ISYS6299035 - Business Process Fundamental</option>
+                                                        <option value="ISYS6300035">ISYS6300035 - Enterprise Systems</option>
+                                                        <option value="ISYS6305035">ISYS6305035 - Data and Information Management</option>
+                                                    </select>
+                                                </div>
+                                                <div class="form-group">
+                                                    <label for="sks">Jumlah SKS</label>
+                                                    <input type="text" class="form-control" placeholder="SKS" name="sks">
+                                                </div>
+                                                <div class="form-group">
+                                                    <label for="kelas">Kelas</label>
+                                                    <input type="text" class="form-control" placeholder="Kelas" name="kelas">
+                                                </div>
+                                                <div class="form-group">
+                                                    <label for="dosen1">Nama Dosen</label>
+                                                    <input type="text" class="form-control" name="dosen1" placeholder="Nama Dosen" disabled>
+                                                </div>
+                                                <div class="form-group">
+                                                    <label for="author">Sebagai dosen tunggal?</label>
+                                                    <div class="row skin skin-flat" style="margin-left: initial;">
+                                                        <div>
+                                                            <fieldset>
+                                                                <input name="author" type="radio" name="input-radio-4" id="input-radio-15" value="yes">
+                                                                <label for="input-radio-15">Yes</label>
+                                                            </fieldset>
+                                                            <fieldset>
+                                                                <input name="author" type="radio" name="input-radio-4" id="input-radio-15" value="no">
+                                                                <label for="input-radio-16">No</label>
+                                                            </fieldset>
                                                         </div>
                                                     </div>
                                                 </div>
-
-                                                <button type="button" data-repeater-create class="btn btn-primary">
-                                                    <i class="ft-plus"></i> Add another contact
-                                                </button>
-                                            </div>
-
-                                        </form>
+                                                <div class="form-group">
+                                                    <!-- <input type="text" class="form-control" placeholder="Kode Matkul" name="first"> -->
+                                                    <label for="dosen2">Dosen 2</label>
+                                                    <select id="dosen2" name="dosen2" class="form-control">
+                                                        <option value="none" selected="" disabled="">Dosen 2</option>
+                                                        <option value="ISYS6327035">D4567 - Titan, S.Kom ., MMSI.</option>
+                                                        <option value="ISYS6296038">D5001 - Hendro, S.Kom., M.M</option>
+                                                        <option value="ISYS6299035">D5088 - Dr. Feri Sulianta, S.T., M.T.I</option>
+                                                        <option value="ISYS6300035">ISYS6300035 - Enterprise Systems</option>
+                                                        <option value="ISYS6305035">ISYS6305035 - Data and Information Management</option>
+                                                    </select>
+                                                </div>
+                                            </form>
+                                        </div>
                                     </div>
                                 </div>
                             </div>
@@ -164,6 +165,34 @@
     <!-- BEGIN PAGE LEVEL JS-->
     <script src="<?php echo base_url(); ?>app-assets/js/scripts/forms/form-repeater.js" type="text/javascript"></script>
     <!-- END PAGE LEVEL JS-->
+    <script>
+        var publikasi = "";
+        var x = document.getElementById("int-journal");
+        var y = document.getElementById("nas-journal");
+        var z = document.getElementById("int-con");
+
+        $('#publikasi').change(function() {
+            if ($(this).val() == 'International Journal') {
+                var publikasi = "International Journal";
+                x.style.display = "contents";
+                y.style.display = "none";
+                z.style.display = "none";
+            } else if ($(this).val() == 'Nasional Journal') {
+                var publikasi = "Nasional Journal";
+                x.style.display = "none";
+                y.style.display = "contents";
+                z.style.display = "none";
+            } else if ($(this).val() == 'International Conference') {
+                var publikasi = "International Conference";
+                x.style.display = "none";
+                y.style.display = "none";
+                z.style.display = "contents";
+            } else {
+                var publikasi = "";
+            }
+            console.log(publikasi);
+        });
+    </script>
 </body>
 
 </html>
