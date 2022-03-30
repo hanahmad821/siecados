1f88bc658874ea72e262e4f1047ce644da957c7d
hanahmad821 <hanahmad821@gmail.com>
2022-03-30 18:57:39 +0700

    edit js form bidang b

--- application/views/bidangB.php
+++ application/views/bidangB.php
@@ -407,31 +407,6 @@
         <?php $this->load->view("_partials/footer.php") ?>
     </footer>
 
-    <!-- BEGIN VENDOR JS-->
-    <script src="<?php echo base_url(); ?>app-assets/vendors/js/vendors.min.js" type="text/javascript"></script>
-    <!-- BEGIN VENDOR JS-->
-    <!-- BEGIN PAGE VENDOR JS-->
-    <script src="<?php echo base_url(); ?>app-assets/vendors/js/forms/repeater/jquery.repeater.min.js" type="text/javascript"></script>
-    <script src="<?php echo base_url(); ?>app-assets/vendors/js/forms/icheck/icheck.min.js" type="text/javascript"></script>
-    <script src="<?php echo base_url(); ?>app-assets/vendors/js/pickers/pickadate/picker.js" type="text/javascript"></script>
-    <script src="<?php echo base_url(); ?>app-assets/vendors/js/pickers/pickadate/picker.date.js" type="text/javascript"></script>
-    <script src="<?php echo base_url(); ?>app-assets/vendors/js/pickers/pickadate/picker.time.js" type="text/javascript"></script>
-    <script src="<?php echo base_url(); ?>app-assets/vendors/js/pickers/pickadate/legacy.js" type="text/javascript"></script>
-    <script src="<?php echo base_url(); ?>app-assets/vendors/js/pickers/dateTime/moment-with-locales.min.js" type="text/javascript"></script>
-    <script src="<?php echo base_url(); ?>app-assets/vendors/js/pickers/daterange/daterangepicker.js" type="text/javascript"></script>
-    <!-- END PAGE VENDOR JS-->
-    <!-- BEGIN CHAMELEON  JS-->
-    <script src="<?php echo base_url(); ?>app-assets/js/core/app-menu.js" type="text/javascript"></script>
-    <script src="<?php echo base_url(); ?>app-assets/js/core/app.js" type="text/javascript"></script>
-    <!-- END CHAMELEON  JS-->
-    <!-- BEGIN PAGE LEVEL JS-->
-    <script src="<?php echo base_url(); ?>app-assets/js/scripts/forms/form-repeater.js" type="text/javascript"></script>
-    <script src="<?php echo base_url(); ?>app-assets/js/scripts/forms/checkbox-radio.js" type="text/javascript"></script>
-    <script src="<?php echo base_url(); ?>app-assets/js/scripts/pickers/dateTime/pick-a-datetime.js" type="text/javascript"></script>
-
-
-    <!-- END PAGE LEVEL JS-->
-
     <script>
         var publikasi = "";
         var x = document.getElementById("int-journal");
