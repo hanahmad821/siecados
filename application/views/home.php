0ff6480c770b4d7d5e44dd77a364b846b4d34e77
hanahmad821 <hanahmad821@gmail.com>
2022-04-18 22:35:56 +0700

    edit home

--- application/views/home.php
+++ application/views/home.php
@@ -661,23 +661,5 @@
     <footer class="footer footer-static footer-light navbar-border navbar-shadow">
     <?php $this->load->view("_partials/footer.php") ?>
     </footer>
-
-    <!-- BEGIN VENDOR JS-->
-    <script src="<?php echo base_url();?>app-assets/vendors/js/vendors.min.js" type="text/javascript"></script>
-    <!-- BEGIN VENDOR JS-->
-    <!-- BEGIN PAGE VENDOR JS-->
-    <script src="<?php echo base_url();?>app-assets/vendors/js/charts/chartist.min.js" type="text/javascript"></script>
-    <script src="<?php echo base_url();?>app-assets/vendors/js/charts/chartist-plugin-tooltip.min.js" type="text/javascript"></script>
-    <script src="<?php echo base_url();?>app-assets/vendors/js/timeline/horizontal-timeline.js" type="text/javascript"></script>
-    <script src="<?php echo base_url();?>app-assets/vendors/js/extensions/dropzone.min.js" type="text/javascript"></script>
-    <script src="<?php echo base_url();?>app-assets/vendors/js/forms/icheck/icheck.min.js" type="text/javascript"></script>
-    <!-- END PAGE VENDOR JS-->
-    <!-- BEGIN CHAMELEON  JS-->
-    <script src="<?php echo base_url();?>app-assets/js/core/app-menu.js" type="text/javascript"></script>
-    <script src="<?php echo base_url();?>app-assets/js/core/app.js" type="text/javascript"></script>
-    <!-- END CHAMELEON  JS-->
-    <!-- BEGIN PAGE LEVEL JS-->
-    <script src="<?php echo base_url();?>app-assets/js/scripts/pages/dashboard-ecommerce.js" type="text/javascript"></script>
-    <!-- END PAGE LEVEL JS-->
   </body>
 </html>
