340cc7b1e5681b0e2c617b17ecc56f4ee1483dba
hanahmad821 <hanahmad821@gmail.com>
2022-04-18 22:35:48 +0700

    edit sidebar

--- application/views/_partials/sidebar.php
+++ application/views/_partials/sidebar.php
@@ -1,67 +1,44 @@
 <?php
 ?>
-<div class="main-menu menu-fixed menu-dark menu-accordion menu-shadow" data-scroll-to-active="true" data-img="<?php echo base_url();?>app-assets/images/backgrounds/04.jpg">
-      <div class="navbar-header">
-        <ul class="nav navbar-nav flex-row position-relative">       
-          <li class="nav-item mr-auto"><a class="navbar-brand" href="<?php echo base_url();?>index.html"><img class="brand-logo" alt="Chameleon admin logo" src="<?php echo base_url();?>app-assets/images/logo/logo-binus.png"/>
-              <h3 class="brand-text">Siecados</h3></a></li>
-          <li class="nav-item d-none d-md-block nav-toggle"><a class="nav-link modern-nav-toggle pr-0" data-toggle="collapse"><i class="toggle-icon ft-disc font-medium-3" data-ticon="ft-disc"></i></a></li>
-          <li class="nav-item d-md-none"><a class="nav-link close-navbar"><i class="ft-x"></i></a></li>
-        </ul>
-      </div>
-      <div class="main-menu-content">
-        <ul class="navigation navigation-main" id="main-menu-navigation" data-menu="menu-navigation">
-          <li class=" nav-item"><a href="<?php echo base_url();?>index.html"><i class="ft-book"></i><span class="menu-title" data-i18n="">Dashboard</span><span class="badge badge badge-info badge-pill float-right mr-2">3</span></a>
-            <ul class="menu-content">
-              <li class="active"><a class="menu-item" href="<?php echo base_url();?>dashboard-ecommerce.html">eCommerce</a>
-              </li>
-              <li><a class="menu-item" href="<?php echo base_url();?>dashboard-analytics.html">Analytics</a>
-              </li>
-            </ul>
+<div class="main-menu menu-fixed menu-dark menu-accordion menu-shadow" data-scroll-to-active="true" data-img="<?php echo base_url(); ?>app-assets/images/backgrounds/04.jpg">
+  <div class="navbar-header">
+    <ul class="nav navbar-nav flex-row position-relative">
+      <li class="nav-item mr-auto"><a class="navbar-brand" href="<?php echo base_url(); ?>index.html"><img class="brand-logo" alt="Chameleon admin logo" src="<?php echo base_url(); ?>app-assets/images/logo/logo-binus.png" />
+          <h3 class="brand-text">Siecados</h3>
+        </a></li>
+      <li class="nav-item d-none d-md-block nav-toggle"><a class="nav-link modern-nav-toggle pr-0" data-toggle="collapse"><i class="toggle-icon ft-disc font-medium-3" data-ticon="ft-disc"></i></a></li>
+      <li class="nav-item d-md-none"><a class="nav-link close-navbar"><i class="ft-x"></i></a></li>
+    </ul>
+  </div>
+  <div class="main-menu-content">
+    <ul class="navigation navigation-main" id="main-menu-navigation" data-menu="menu-navigation">
+      <li class=" nav-item"><a href="<?php echo base_url(); ?>index.html"><i class="ft-book"></i><span class="menu-title" data-i18n="">Dashboard</span><span class="badge badge badge-info badge-pill float-right mr-2">3</span></a>
+        <ul class="menu-content">
+          <li class="active"><a class="menu-item" href="<?php echo base_url(); ?>dashboard-ecommerce.html">eCommerce</a>
           </li>
-          <li class=" nav-item"><a href="<?php echo base_url();?>index.html"><i class="ft-book"></i><span class="menu-title" data-i18n="">Bidang A</span></a>
-            <ul class="menu-content">
-              <li><a class="menu-item" href="<?php echo base_url();?>BidangA">Form Input</a>
-              </li>
-              <li><a class="menu-item" href="<?php echo base_url();?>dashboard-analytics.html">Table</a>
-              </li>
-            </ul>
-          </li>
-          <li class=" nav-item"><a href="<?php echo base_url();?>index.html"><i class="ft-book"></i><span class="menu-title" data-i18n="">Bidang B</span></a>
-            <ul class="menu-content">
-              <li><a class="menu-item" href="<?php echo base_url();?>BidangB">Form Input</a>
-              </li>
-              <li><a class="menu-item" href="<?php echo base_url();?>dashboard-analytics.html">Table</a>
-              </li>
-            </ul>
-          </li>
-          <li class=" nav-item"><a href="<?php echo base_url();?>index.html"><i class="ft-book"></i><span class="menu-title" data-i18n="">Bidang C</span></a>
-            <ul class="menu-content">
-              <li><a class="menu-item" href="<?php echo base_url();?>BidangC">Form Input</a>
-              </li>
-              <li><a class="menu-item" href="<?php echo base_url();?>dashboard-analytics.html">Table</a>
-              </li>
-            </ul>
-          </li>
-          <li class=" nav-item"><a href="<?php echo base_url();?>index.html"><i class="ft-book"></i><span class="menu-title" data-i18n="">Bidang D</span></a>
-            <ul class="menu-content">
-              <li><a class="menu-item" href="<?php echo base_url();?>BidangD">Form Input</a>
-              </li>
-              <li><a class="menu-item" href="<?php echo base_url();?>dashboard-analytics.html">Table</a>
-              </li>
-            </ul>
-          </li>
-          <li class=" nav-item"><a href="<?php echo base_url();?>index.html"><i class="ft-book"></i><span class="menu-title" data-i18n="">Admin</span></a>
-            <ul class="menu-content">
-              <li><a class="menu-item" href="<?php echo base_url();?>admin/User">User</a>
-              </li>
-              <li><a class="menu-item" href="<?php echo base_url();?>admin/Matkul">Matkul</a>
-              </li>
-              <li><a class="menu-item" href="<?php echo base_url();?>admin/Periode">Periode</a>
-              </li>
-            </ul>
+          <li><a class="menu-item" href="<?php echo base_url(); ?>dashboard-analytics.html">Analytics</a>
           </li>
         </ul>
-      </div>
-      <div class="navigation-background"></div>
-    </div>
+      </li>
+      <li class=" nav-item"><a href="<?php echo base_url(); ?>BidangA"><i class="ft-book"></i><span class="menu-title" data-i18n="">Bidang A</span></a>
+      </li>
+      <li class=" nav-item"><a href="<?php echo base_url(); ?>BidangB"><i class="ft-book"></i><span class="menu-title" data-i18n="">Bidang B</span></a>
+      </li>
+      <li class=" nav-item"><a href="<?php echo base_url(); ?>BidangC"><i class="ft-book"></i><span class="menu-title" data-i18n="">Bidang C</span></a>
+      </li>
+      <li class=" nav-item"><a href="<?php echo base_url(); ?>BidangD"><i class="ft-book"></i><span class="menu-title" data-i18n="">Bidang D</span></a>
+      </li>
+      <li class=" nav-item"><a href="<?php echo base_url(); ?>index.html"><i class="ft-book"></i><span class="menu-title" data-i18n="">Admin</span></a>
+        <ul class="menu-content">
+          <li><a class="menu-item" href="<?php echo base_url(); ?>admin/User">User</a>
+          </li>
+          <li><a class="menu-item" href="<?php echo base_url(); ?>admin/Matkul">Matkul</a>
+          </li>
+          <li><a class="menu-item" href="<?php echo base_url(); ?>admin/Periode">Periode</a>
+          </li>
+        </ul>
+      </li>
+    </ul>
+  </div>
+  <div class="navigation-background"></div>
+</div>
